69add52fda7f2c8c4ae055a8e56986c02f55bcab
lucIFER24122001 <soumyamohanty20202020@gmail.com>
2024-03-01 23:16:08 +0530

    SRM-5

--- prg9.java
+++ prg9.java
@@ -3,6 +3,26 @@ public class prg9 {
 
     public static void main(String args[])
     {
+        Scanner sc = new Scanner(System.in);
+        int n =  sc.nextInt();
+        int nums[] =  new int[n];
+        for(int i = 0;i< nums.length;i++)
+        {
+            nums[i] =  sc.nextInt();
+        }
+
+        System.out.println(majority(nums));
 
     }
+
+    public static int majority(int[] nums)
+    {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int ans = nums[n/2];
+
+        return n;
+    }
+
+
 }
